4713d939c06a2e22a3ff8d80681cf013408777ab
Abdul rehman <rehman.20497@gmail.com>
2025-12-30 13:21:28 +0000

    split this row into 2 column and place map section in left column withou

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,21 @@ import UsaMap from "@/components/usa-map";
 
 export default function Home() {
   return (
-    <main className="relative flex h-screen w-screen items-center justify-center bg-background font-body overflow-hidden">
-      <div className="absolute inset-0 z-0">
-        <UsaMap />
+    <main className="flex h-screen w-screen bg-background font-body overflow-hidden">
+      {/* Left Column: Map */}
+      <div className="relative flex-1 flex items-center justify-center overflow-hidden">
+        <div className="absolute inset-0 z-0">
+          <UsaMap />
+        </div>
+        <div className="absolute inset-0 z-10 bg-[radial-gradient(ellipse_at_center,transparent_20%,hsl(var(--background)))]"></div>
       </div>
-      <div className="absolute inset-0 z-10 bg-[radial-gradient(ellipse_at_center,transparent_20%,hsl(var(--background)))]"></div>
-      <div className="z-20">
-        <TestimonialNetwork />
+
+      {/* Right Column: Testimonials */}
+      <div className="relative flex-1 flex items-center justify-center">
+        <div className="absolute inset-0 z-10 bg-[radial-gradient(ellipse_at_center,transparent_60%,hsl(var(--background)))]"></div>
+        <div className="z-20">
+          <TestimonialNetwork />
+        </div>
       </div>
     </main>
   );
