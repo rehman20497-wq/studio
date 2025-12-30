0b49643808a5c0dd6a77d47681ed7cf02f6d7f2b
Abdul rehman <rehman.20497@gmail.com>
2025-12-30 12:47:41 +0000

    background is not visible add USA abstract map map sure it should be act

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import TestimonialNetwork from "@/components/testimonial-network";
-import IconCloud from "@/components/icon-cloud";
+import UsaMap from "@/components/usa-map";
 
 export default function Home() {
   return (
     <main className="relative flex h-screen w-screen items-center justify-center bg-background font-body overflow-hidden">
       <div className="absolute inset-0 z-0">
-        <IconCloud />
+        <UsaMap />
       </div>
       <div className="absolute inset-0 z-10 bg-[radial-gradient(ellipse_at_center,transparent_20%,hsl(var(--background)))]"></div>
       <div className="z-20">
