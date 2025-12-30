0de5343955013c93e7130bd2205a9d45a67355a2
Abdul rehman <rehman.20497@gmail.com>
2025-12-30 12:41:42 +0000

    add some attractive background behind our testimonials network

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import TestimonialNetwork from "@/components/testimonial-network";
 
 export default function Home() {
   return (
-    <main className="relative flex h-screen w-screen items-center justify-center bg-background font-body">
-      <TestimonialNetwork />
+    <main className="relative flex h-screen w-screen items-center justify-center bg-background font-body overflow-hidden">
+      <div className="absolute inset-0 z-0 bg-background">
+        <div className="absolute inset-0 z-10 bg-[radial-gradient(ellipse_at_center,transparent_20%,hsl(var(--background)))]"></div>
+      </div>
+      <div className="z-10">
+        <TestimonialNetwork />
+      </div>
     </main>
   );
 }
