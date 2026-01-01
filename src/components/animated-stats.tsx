02dac81407be461998b86975756c05711c6b9cd9
Abdul rehman <rehman.20497@gmail.com>
2026-01-01 15:34:49 +0000

    now add ticket response time in a separate row or grid and show it in ce

--- src/components/animated-stats.tsx
+++ src/components/animated-stats.tsx
@@ -84,73 +84,75 @@ export default function AnimatedStats() {
                 </svg>
             </div>
             
-            <div className="relative w-full mx-auto mt-12 grid grid-cols-10 items-center gap-[5%]">
-                <div className="col-span-3">
-                    <div className="relative w-48 h-48 mx-auto">
-                        <div className="absolute -left-4 top-0 animate-float-1">
-                            <FloatingIcon>
-                                <svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M14 0L17.1432 10.1329L27.4894 10.1329L19.1731 16.3942L22.3163 26.527L14 20.2658L5.68369 26.527L8.82689 16.3942L0.51059 10.1329L10.8568 10.1329L14 0Z" fill="black"/>
-                                </svg>
-                            </FloatingIcon>
-                        </div>
-                        <div className="absolute right-0 top-8 animate-float-2">
-                            <FloatingIcon>
-                                <svg width="30" height="24" viewBox="0 0 30 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M29.5 7.25C29.5 12.4875 25.125 21.25 15.25 23C5.375 21.25 1 12.4875 1 7.25C1 2.5 5.375 1 9.75 1C14.125 1 13 4.75 15.25 7.25C17.5 4.75 18.375 1 22.75 1C27.125 1 29.5 2.5 29.5 7.25Z" stroke="black" strokeWidth="2"/>
-                                </svg>
-                            </FloatingIcon>
-                        </div>
-                        <div className="absolute right-4 bottom-0 animate-float-3">
-                            <FloatingIcon>
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" stroke="black" strokeWidth="2"/>
-                                </svg>
-                            </FloatingIcon>
-                        </div>
+            <div>
+                <div className="relative w-full mx-auto mt-12 grid grid-cols-10 items-center gap-[5%]">
+                    <div className="col-span-3">
+                        <div className="relative w-48 h-48 mx-auto">
+                            <div className="absolute -left-4 top-0 animate-float-1">
+                                <FloatingIcon>
+                                    <svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M14 0L17.1432 10.1329L27.4894 10.1329L19.1731 16.3942L22.3163 26.527L14 20.2658L5.68369 26.527L8.82689 16.3942L0.51059 10.1329L10.8568 10.1329L14 0Z" fill="black"/>
+                                    </svg>
+                                </FloatingIcon>
+                            </div>
+                            <div className="absolute right-0 top-8 animate-float-2">
+                                <FloatingIcon>
+                                    <svg width="30" height="24" viewBox="0 0 30 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M29.5 7.25C29.5 12.4875 25.125 21.25 15.25 23C5.375 21.25 1 12.4875 1 7.25C1 2.5 5.375 1 9.75 1C14.125 1 13 4.75 15.25 7.25C17.5 4.75 18.375 1 22.75 1C27.125 1 29.5 2.5 29.5 7.25Z" stroke="black" strokeWidth="2"/>
+                                    </svg>
+                                </FloatingIcon>
+                            </div>
+                            <div className="absolute right-4 bottom-0 animate-float-3">
+                                <FloatingIcon>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" stroke="black" strokeWidth="2"/>
+                                    </svg>
+                                </FloatingIcon>
+                            </div>
 
-                        <div className="absolute inset-0 m-auto flex items-center justify-center">
-                            <div className="relative rounded-full bg-gradient-to-b from-[#00B4C6] to-[#40E0D0] p-1">
-                                <div className="bg-white rounded-full p-[5px]">
-                                    <div className="relative w-[70px] h-[70px] rounded-full overflow-hidden">
-                                        <Image
-                                            src="https://images.unsplash.com/photo-1594672830234-ba4cfe1202dc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMHx8bWFuJTIwcG9ydHJhaXR8ZW58MHx8fHwxNzY3MDEwODkzfDA&ixlib=rb-4.1.0&q=80&w=1080"
-                                            alt="Agent Portrait"
-                                            fill
-                                            sizes="(max-width: 768px) 100vw, 33vw"
-                                            className="object-cover"
-                                            data-ai-hint="man portrait"
-                                        />
+                            <div className="absolute inset-0 m-auto flex items-center justify-center">
+                                <div className="relative rounded-full bg-gradient-to-b from-[#00B4C6] to-[#40E0D0] p-[5px]">
+                                    <div className="bg-white rounded-full p-[5px]">
+                                        <div className="relative w-[70px] h-[70px] rounded-full overflow-hidden">
+                                            <Image
+                                                src="https://images.unsplash.com/photo-1594672830234-ba4cfe1202dc?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMHx8bWFuJTIwcG9ydHJhaXR8ZW58MHx8fHwxNzY3MDEwODkzfDA&ixlib=rb-4.1.0&q=80&w=1080"
+                                                alt="Agent Portrait"
+                                                fill
+                                                sizes="(max-width: 768px) 100vw, 33vw"
+                                                className="object-cover"
+                                                data-ai-hint="man portrait"
+                                            />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="col-span-7 mr-[10%] pl-[5%]">
+                    <div className="col-span-7 mr-[10%] pl-[5%]">
+                        <StatCard
+                            title="Accuracy Score"
+                            value="DATA 1"
+                            percentage="+10%"
+                            progress={75}
+                            percentageColor="bg-[#9AFEFF]"
+                            path="M0,0 C15,0 15,30 25,30"
+                            className="w-full"
+                        />
+                    </div>
+                </div>
+                <div className='relative w-full mt-10 flex justify-center'>
                     <StatCard
-                        title="Accuracy Score"
+                        title="Ticket Response Time"
                         value="DATA 1"
-                        percentage="+10%"
-                        progress={75}
+                        percentage="-31%"
+                        time="08 MIN 34 SEC"
+                        progress={40}
                         percentageColor="bg-[#9AFEFF]"
-                        path="M0,0 C15,0 15,30 25,30"
-                        className="w-[300px]"
+                        path="M25,0 C15,0 15,30 0,30"
+                        className="w-[350px]"
                     />
                 </div>
-            </div>
-             <div className='relative w-fit mx-auto'>
-                <StatCard
-                    title="Ticket Response Time"
-                    value="DATA 1"
-                    percentage="-31%"
-                    time="08 MIN 34 SEC"
-                    progress={40}
-                    percentageColor="bg-[#9AFEFF]"
-                    path="M25,0 C15,0 15,30 0,30"
-                    className="top-full mt-10 left-full -ml-8 w-[350px]"
-                />
             </div>
         </div>
     );
