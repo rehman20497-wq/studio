6b340e418cfc8c98d3d3973383c625bf626dfbf0
Abdul rehman <rehman.20497@gmail.com>
2026-01-02 18:05:10 +0000

    forget it lets start one by one now first create one arc or half circle

--- src/components/icons/customer-support-icon.tsx
+++ src/components/icons/customer-support-icon.tsx
@@ -6,37 +6,11 @@ export const CustomerSupportIcon = ({ className = '' }) => (
     className={`block w-[140px] h-[140px] ${className}`}
     preserveAspectRatio="xMidYMid meet"
   >
-    <g transform="rotate(50 32 32)">
-      <path
-        className="customer-support-arc customer-support-arc-color"
-        d="M14.5 33 A19 19 0 0 1 49.5 33"
-        strokeWidth="6"
-        strokeLinecap="round"
-        style={{
-          strokeDasharray: 104,
-          strokeDashoffset: 104,
-        }}
-      />
-      <path
-        className="customer-support-arc customer-support-arc-color"
-        d="M22 33 A12 12 0 0 1 42 33"
-        strokeWidth="6"
-        strokeLinecap="round"
-        style={{
-          strokeDasharray: 63,
-          strokeDashoffset: 63,
-        }}
-      />
-      <path
-        className="customer-support-arc customer-support-arc-color"
-        d="M27.5 33 A6 6 0 0 1 36.5 33"
-        strokeWidth="6"
-        strokeLinecap="round"
-        style={{
-          strokeDasharray: 29,
-          strokeDashoffset: 29,
-        }}
-      />
-    </g>
+    <path
+      d="M14.5 49.5 A 22 22 0 0 1 49.5 49.5"
+      stroke="#06B6D4"
+      strokeWidth="12"
+      strokeLinecap="round"
+    />
   </svg>
 );
