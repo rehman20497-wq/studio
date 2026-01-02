40f27e59f2fd24eec0a1e9f4c791902d6dd008d3
Abdul rehman <rehman.20497@gmail.com>
2026-01-02 21:58:02 +0000

    increase view point to 140px

--- src/components/icons/trust-safety-icon.tsx
+++ src/components/icons/trust-safety-icon.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export const TrustSafetyIcon = () => (
-  <svg width="100" height="75" viewBox="0 0 100 75" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g transform="rotate(50 50 37.5)">
-      <circle cx="37" cy="37.5" r="33.5" stroke="#A78BFA" strokeWidth="8"/>
-      <circle cx="63" cy="37.5" r="33.5" stroke="#A78BFA" strokeWidth="8"/>
+  <svg width="140" height="105" viewBox="0 0 140 105" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g transform="rotate(50 70 52.5)">
+      <circle cx="52" cy="52.5" r="33.5" stroke="#A78BFA" strokeWidth="8"/>
+      <circle cx="88" cy="52.5" r="33.5" stroke="#A78BFA" strokeWidth="8"/>
     </g>
   </svg>
 );
