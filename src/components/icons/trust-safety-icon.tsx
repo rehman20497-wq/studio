3cad0a8855aab22e92547b76b961becb2a11d474
Abdul rehman <rehman.20497@gmail.com>
2026-01-02 21:54:32 +0000

    now create new component separate in which we have two same size circle

--- src/components/icons/trust-safety-icon.tsx
+++ src/components/icons/trust-safety-icon.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 export const TrustSafetyIcon = () => (
-  <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="26" cy="26" r="22" stroke="#A78BFA" strokeWidth="8"/>
-    <path d="M38 38L60 60" stroke="#A78BFA" strokeWidth="8" strokeLinecap="round" strokeOpacity="0.5"/>
-    <circle cx="38" cy="38" r="22" stroke="#A78BFA" strokeWidth="8"/>
+  <svg width="88" height="64" viewBox="0 0 88 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="32" cy="32" r="28" stroke="#A78BFA" strokeWidth="8"/>
+    <circle cx="56" cy="32" r="28" stroke="#A78BFA" strokeWidth="8"/>
   </svg>
 );
