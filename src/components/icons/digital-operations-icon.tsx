1a850e98e61523940cac02363cb58eabb4a00e62
Abdul rehman <rehman.20497@gmail.com>
2026-01-03 11:50:15 +0000

    now move on to digital operations icon create separate file in this circ

--- src/components/icons/digital-operations-icon.tsx
+++ src/components/icons/digital-operations-icon.tsx
@@ -1,13 +1,47 @@
 import React from 'react';
 
-export const DigitalOperationsIcon = () => (
-  <svg width="72" height="72" viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="18" cy="18" r="14" stroke="#4ADE80" strokeWidth="8"/>
-    <circle cx="54" cy="18" r="14" stroke="#4ADE80" strokeWidth="8"/>
-    <circle cx="18" cy="54" r="14" stroke="#4ADE80" strokeWidth="8"/>
-    <circle cx="54"cy="54" r="14" stroke="#4ADE80" strokeWidth="8"/>
-    <path d="M18 18 a 14 14 0 0 1 0 -28 a 14 14 0 0 1 0 28" fill="#4ADE80" stroke="none">
-        <animate attributeName="d" from="M18 18 a 14 14 0 0 1 0 -28 a 14 14 0 0 1 0 28" to="M18 18 a 14 14 0 1 1 0 -28 a 14 14 0 0 1 0 28" dur="2s" begin="0s" repeatCount="indefinite" />
-    </path>
-  </svg>
-);
+export const DigitalOperationsIcon = () => {
+    const size = 130;
+    const circleRadius = size / 4;
+    const strokeWidth = 14;
+    const innerRadius = circleRadius - strokeWidth / 2;
+    const circumference = 2 * Math.PI * innerRadius;
+    const fillPercentage = 75;
+    const strokeDashoffset = circumference * (1 - fillPercentage / 100);
+
+    const positions = [
+        { cx: circleRadius, cy: circleRadius },
+        { cx: size - circleRadius, cy: circleRadius },
+        { cx: circleRadius, cy: size - circleRadius },
+        { cx: size - circleRadius, cy: size - circleRadius },
+    ];
+
+    return (
+        <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`} fill="none" xmlns="http://www.w3.org/2000/svg">
+            {/* Bottom-left circle */}
+            <circle cx={positions[2].cx} cy={positions[2].cy} r={innerRadius} stroke="#A7F3D0" strokeWidth={strokeWidth} />
+            {/* Bottom-right circle */}
+            <circle cx={positions[3].cx} cy={positions[3].cy} r={innerRadius} stroke="#A7F3D0" strokeWidth={strokeWidth} />
+            {/* Top-right circle */}
+            <circle cx={positions[1].cx} cy={positions[1].cy} r={innerRadius} stroke="#A7F3D0" strokeWidth={strokeWidth} />
+            
+            {/* Top-left circle (75% filled) */}
+            <g>
+                {/* Background ring */}
+                <circle cx={positions[0].cx} cy={positions[0].cy} r={innerRadius} stroke="#A7F3D0" strokeWidth={strokeWidth} />
+                {/* Filled portion */}
+                <circle
+                    cx={positions[0].cx}
+                    cy={positions[0].cy}
+                    r={innerRadius}
+                    stroke="#34D399"
+                    strokeWidth={strokeWidth}
+                    strokeDasharray={circumference}
+                    strokeDashoffset={strokeDashoffset}
+                    strokeLinecap="round"
+                    transform={`rotate(-90 ${positions[0].cx} ${positions[0].cy})`}
+                />
+            </g>
+        </svg>
+    );
+};
