e16dffdf8ed738d0ba11878791dd7b1f65cdd2d2
Abdul rehman <rehman.20497@gmail.com>
2026-01-03 12:06:18 +0000

    why you remove animation which we set for this icon??? please check and

--- src/components/icons/customer-support-icon.tsx
+++ src/components/icons/customer-support-icon.tsx
@@ -17,30 +17,46 @@ export const CustomerSupportIcon = ({ className = '' }) => (
       </g>
       
       {/* Animated Fluid Layer */}
-      <g strokeWidth="4" fill="none" strokeLinecap="round">
+      <g className="stroke-theme-primary" strokeWidth="4" fill="none" strokeLinecap="round">
         <path
           className="fluid-arc"
           d="M32,10 A22,22 0 0 1 53.66,32"
+          strokeDasharray="47.5"
+          strokeDashoffset="47.5"
         />
         <path
           className="fluid-arc"
           d="M32,10 A22,22 0 0 0 10.34,32"
+          strokeDasharray="47.5"
+          strokeDashoffset="47.5"
         />
         <path
-          className="fluid-arc delay-150"
+          className="fluid-arc"
           d="M32,16 A16,16 0 0 1 47.76,32"
+          strokeDasharray="34.5"
+          strokeDashoffset="34.5"
+          style={{ transitionDelay: '0.15s' }}
         />
         <path
-          className="fluid-arc delay-150"
+          className="fluid-arc"
           d="M32,16 A16,16 0 0 0 16.24,32"
+          strokeDasharray="34.5"
+          strokeDashoffset="34.5"
+          style={{ transitionDelay: '0.15s' }}
         />
         <path
-          className="fluid-arc delay-300"
+          className="fluid-arc"
           d="M32,22 A10,10 0 0 1 41.85,32"
+          strokeDasharray="21.6"
+          strokeDashoffset="21.6"
+          style={{ transitionDelay: '0.3s' }}
         />
         <path
-          className="fluid-arc delay-300"
+          className="fluid-arc"
           d="M32,22 A10,10 0 0 0 22.15,32"
+          strokeDasharray="21.6"
+          strokeDashoffset="21.6"
+          style={{ transitionDelay: '0.3s' }}
         />
       </g>
     </g>
