af144acf06071e70a22d78b16ddda4788a58e71d
Abdul rehman <rehman.20497@gmail.com>
2026-01-06 17:45:48 +0000

    what are you doing?? all these strokes are overlapping each other create

--- src/components/about/our-values-section.tsx
+++ src/components/about/our-values-section.tsx
@@ -45,7 +45,7 @@ const BrushStroke = ({ d, color, delay, y }: { d: string, color: string; delay: 
             ref={ref}
             viewBox="0 0 1440 100"
             preserveAspectRatio="none"
-            className="absolute w-full h-auto"
+            className="absolute w-full h-auto left-0"
             style={{ top: y, height: '80px' }}
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -97,20 +97,19 @@ const PlayButton = () => (
 )
 
 const strokes = [
-    { d: "M-20 50 C 320 20, 1120 80, 1460 50", color: '#fff9e6', delay: 0, y: 0 },
-    { d: "M-20 50 C 420 80, 1020 20, 1460 50", color: '#fef08a', delay: 0.1, y: 30 },
-    { d: "M-20 50 C 320 30, 1120 70, 1460 50", color: '#bbf7d0', delay: 0.2, y: 60 },
-    { d: "M-20 50 C 420 70, 1020 30, 1460 50", color: '#a5f3fc', delay: 0.3, y: 90 },
-    { d: "M-20 50 C 320 40, 1120 60, 1460 50", color: '#fecaca', delay: 0.4, y: 120 },
+    { d: "M-20 50 C 320 20, 1120 80, 1460 50", color: '#fef08a', delay: 0.1, y: 0 },
+    { d: "M-20 50 C 420 80, 1020 20, 1460 50", color: '#bbf7d0', delay: 0.2, y: 90 },
+    { d: "M-20 50 C 320 30, 1120 70, 1460 50", color: '#a5f3fc', delay: 0.3, y: 180 },
+    { d: "M-20 50 C 420 70, 1020 30, 1460 50", color: '#fecaca', delay: 0.4, y: 270 },
 ]
   
 export default function OurValuesSection() {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, amount: 0.4 });
+  const isInView = useInView(ref, { once: true, amount: 0.2 });
 
   return (
     <section ref={ref} className="bg-[#fff9e6] py-32 relative overflow-hidden flex items-center justify-center min-h-[600px]">
-        <div className="absolute inset-0">
+        <div className="absolute inset-x-0 top-1/2 -translate-y-1/2 h-[380px]">
             {strokes.map((s, i) => (
                 <BrushStroke key={i} {...s} />
             ))}
