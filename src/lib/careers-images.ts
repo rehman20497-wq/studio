877506cfe723e8dea803ecab3e0d16d158e116cb
Abdul rehman <rehman.20497@gmail.com>
2026-01-07 11:29:35 +0000

    use stock image please

--- src/lib/careers-images.ts
+++ src/lib/careers-images.ts
@@ -1,43 +1,43 @@
 
 export const careersImages = {
     years: {
-        src: '/careers/c1.png',
+        src: 'https://picsum.photos/seed/career1/220/220',
         hint: 'team working',
         alt: 'Team members working at desks',
         label: 'Years'
     },
     academy: {
-        src: '/careers/c2.png',
+        src: 'https://picsum.photos/seed/career2/240/240',
         hint: 'graduates certificate',
         alt: 'Graduates holding certificates',
         label: 'Hugo Academy \'23'
     },
     day1: {
-        src: '/careers/c3.png',
+        src: 'https://picsum.photos/seed/career3/250/250',
         hint: 'woman smiling',
         alt: 'Woman smiling in front of a colorful wall',
         label: 'Hugo Day 1'
     },
     training: {
-        src: '/careers/c4.png',
+        src: 'https://picsum.photos/seed/career4/230/230',
         hint: 'woman presenting',
         alt: 'Woman presenting in a training session',
         label: 'Training'
     },
     iwd: {
-        src: '/careers/c5.png',
+        src: 'https://picsum.photos/seed/career5/280/280',
         hint: 'team photo',
         alt: 'Group photo of a team',
         label: 'IWD 2022'
     },
     hugo: {
-        src: '/careers/c6.png',
+        src: 'https://picsum.photos/seed/career6/240/240',
         hint: 'team meeting',
         alt: 'Team meeting around a table',
         label: 'Hugo'
     },
     team: {
-        src: '/careers/c7.png',
+        src: 'https://picsum.photos/seed/career7/220/220',
         hint: 'team collaboration',
         alt: 'Team collaborating in an office',
         label: ''
