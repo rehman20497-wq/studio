69933434a7bdb197e160569bd75a87ebf2b4025d
Abdul rehman <rehman.20497@gmail.com>
2026-01-08 12:01:19 +0000

    please add them in our code

---
+++ src/lib/cloudinary.ts
@@ -0,0 +1,9 @@
+// WARNING: Storing secrets directly in source code is not recommended
+// and can lead to security vulnerabilities. These should be stored
+// in environment variables for better security.
+
+export const cloudinaryConfig = {
+  cloudName: 'dm5cqe1f3',
+  apiKey: '899987212698367',
+  apiSecret: 'cnu9824BKAPcDL8--k5wpFPjInQ',
+};
