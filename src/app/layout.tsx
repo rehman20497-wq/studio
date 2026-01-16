9a2b52b16e31158c4c7319e80687a53f00497117
Abdul rehman <rehman.20497@gmail.com>
2026-01-16 20:55:19 +0000

    i have uploaded PP mori font inside fonts folder PP Mori.woff2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,6 @@ export default function RootLayout({
         <link rel="icon" href="/fav.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap" rel="stylesheet" />
       </head>
