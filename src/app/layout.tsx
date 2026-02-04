3e19f865f88125113200e63db43d080ffd316f4f
Abdul rehman <rehman.20497@gmail.com>
2026-02-04 10:01:40 +0000

    hello

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 'use client';
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { usePathname } from 'next/navigation';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -42,6 +43,8 @@ export default function RootLayout({
           <Toaster />
         </FirebaseClientProvider>
         <DisableZoom />
+        <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );
